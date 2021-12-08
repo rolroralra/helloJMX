3dc53cadd042984dd7ec80167011b0f7a35e27d1
Shinyoung Kim <rolroralra@gmail.com>
2021-12-08 18:06:13 +0900

    Update HelloClient.java

--- src/com/example/jmx/HelloClient.java
+++ src/com/example/jmx/HelloClient.java
@@ -1,5 +1,8 @@
 package com.example.jmx;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import javax.management.Attribute;
 import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
@@ -7,18 +10,21 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.util.Arrays;
+import java.util.Hashtable;
 
 public class HelloClient {
-    private String domain;
-    private String ip;
-    private int port;
-    private String contextPath;
+    private final String domain;
+    private final String ip;
+    private final int port;
+    private final String contextPath;
+    private final String jmxServiceUrl;
 
     public HelloClient() {
-        this.domain = "helloDomain";
+        this.domain = "com.example.jmx";
         this.ip = "localhost";
         this.port = 7777;
         this.contextPath = "hello";
+        this.jmxServiceUrl = String.format("service:jmx:rmi:///jndi/rmi://%s:%d/%s", this.ip, this.port, this.contextPath);
     }
 
     public static void main(String[] args) {
@@ -27,7 +33,7 @@ public class HelloClient {
 
     private void foo() {
         try {
-            JMXServiceURL jmxServiceURL = new JMXServiceURL(String.format("service:jmx:rmi:///jndi/rmi://%s:%d/%s", this.ip, this.port, this.contextPath));
+            JMXServiceURL jmxServiceURL = new JMXServiceURL(this.jmxServiceUrl);
 
             JMXConnector jmxConnector = JMXConnectorFactory.connect(jmxServiceURL, null);
 
@@ -39,16 +45,26 @@ public class HelloClient {
                 System.out.println(domain);
             }
 
-            ObjectName helloMBeanName = new ObjectName(String.format("%s:name=helloMBean", this.domain));
+//            ObjectName helloMBeanName = ObjectName.getInstance(this.domain, "name", "helloMBean");
+            ObjectName helloMBeanName = ObjectName.getInstance(this.domain, new Hashtable<String, String>(){{
+                put("name", "helloMBean");
+                put("type", "basic");
+            }});
 
             HelloMBean hello = JMX.newMBeanProxy(mBeanServerConnection, helloMBeanName, HelloMBean.class, true);
 
-            hello.setMessage("Test1234");
+            System.out.println(mBeanServerConnection.getMBeanInfo(helloMBeanName));
+            System.out.println(hello.sayHello());
+
+            hello.setMessage("Hello JMX?!?");
+            System.out.println(hello.sayHello());
+
+//            mBeanServerConnection.setAttribute(helloMBeanName, new Attribute("Message", "????"));
+            mBeanServerConnection.setAttribute(helloMBeanName, new Attribute(StringUtils.capitalize("message"), "????"));
             System.out.println(hello.sayHello());
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
