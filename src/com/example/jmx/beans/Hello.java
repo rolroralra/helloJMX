203845d993addfc1542cf127dea0182fd185e1ea
Shinyoung Kim <rolroralra@gmail.com>
2021-12-08 18:27:07 +0900

    Update and rename src/com/example/jmx/Hello.java to src/com/example/jmx/beans/Hello.java

--- src/com/example/jmx/beans/Hello.java
+++ src/com/example/jmx/beans/Hello.java
@@ -1,17 +1,19 @@
-package com.example.jmx;
+package com.example.jmx.beans;
 
 import java.text.SimpleDateFormat;
 
 public class Hello implements HelloMBean {
 
     private String message;
+    private Integer count;
 
     public Hello() {
-        this("Hello JMX!");
+        this("Hello JMX!", 0);
     }
 
-    public Hello(String message) {
+    public Hello(String message, Integer count) {
         this.message = message;
+        this.count = count;
     }
 
     @Override
@@ -21,6 +23,18 @@ public class Hello implements HelloMBean {
 
     @Override
     public String sayHello() {
+        System.out.println("COUNT: " + this.count);
+        this.count++;
         return String.format("[%s] JMX Message ::: " + this.message, new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()));
+    }
+
+    @Override
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public Integer getCount() {
+        return this.count;
     }
 }
