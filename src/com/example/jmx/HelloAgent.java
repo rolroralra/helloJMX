5838552ab32ddd732c32ed0f7bd506a1ec9b8c05
Shinyoung Kim <rolroralra@gmail.com>
2021-12-08 18:03:02 +0900

    Update HelloAgent.java

--- src/com/example/jmx/HelloAgent.java
+++ src/com/example/jmx/HelloAgent.java
@@ -1,8 +1,6 @@
 package com.example.jmx;
 
-import javax.management.MBeanServer;
-import javax.management.MBeanServerFactory;
-import javax.management.ObjectName;
+import javax.management.*;
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
@@ -10,32 +8,42 @@ import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 
 public class HelloAgent {
-    private MBeanServer mbs;
-    private String domain;
-    private String ip;
-    private int port;
-    private String contextPath;
+    private static HelloMBean helloMBean;
+    private static ObjectName helloMBeanObjectName;
+    private final MBeanServer mbs;
+    private final String domain;
+    private final String ip;
+    private final int port;
+    private final String contextPath;
+    private final String jmxServiceUrl;
 
     public HelloAgent() {
-        this.domain = "helloDomain";
+        this.domain = "com.example.jmx";
         this.ip = "localhost";
         this.port = 7777;
         this.contextPath = "hello";
         this.mbs = MBeanServerFactory.createMBeanServer(this.domain);
+        this.jmxServiceUrl = String.format("service:jmx:rmi:///jndi/rmi://%s:%d/%s", this.ip, this.port, this.contextPath);
+        
+        // JMXServiceURL = "service:jmx:rmi://[ip:port]/jndi/rmi://${ip}:${port}/${contextPath}"
+        // rmi : Remote Method Invocation
+        // jndi : Java Naming and Directory Interface
 
         try {
             LocateRegistry.createRegistry(this.port);
-            // rmi : Remote Method Invocation
-            // jndi : Java Naming and Directory Interface
-            JMXServiceURL serviceUrl = new JMXServiceURL(String.format("service:jmx:rmi:///jndi/rmi://%s:%d/%s", this.ip, this.port, this.contextPath));
 
-            ObjectName helloMBeanName = new ObjectName(String.format("%s:name=helloMBean", this.domain));
+            JMXServiceURL serviceUrl = new JMXServiceURL(jmxServiceUrl);
 
-            Hello helloMBean = new Hello();
-            mbs.registerMBean(helloMBean, helloMBeanName);
+            helloMBeanObjectName= new ObjectName(String.format("%s:type=basic,name=%s", this.domain, "helloMBean"));
+            helloMBean = new Hello();
+
+            mbs.registerMBean(helloMBean, helloMBeanObjectName);
 
             JMXConnectorServer connector = JMXConnectorServerFactory.newJMXConnectorServer(serviceUrl, null, mbs);
 
+            NotificationListener notificationListener = new NotificationListenerImpl();
+            NotificationFilter notificationFilter = (NotificationFilter) notificationListener;
+            connector.addNotificationListener(notificationListener, notificationFilter, new NotificationCallback());
             connector.start();
 
         } catch (Exception e) {
@@ -47,6 +55,7 @@ public class HelloAgent {
         try {
             System.out.println("Press to continue...");
             System.in.read();
+            System.out.println(helloMBean.sayHello());
         } catch (IOException e) {
             e.printStackTrace();
         }
